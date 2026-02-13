80ec7e299746680ad8d18648d0d0b5d9c76b94b3
underthemo-onlight <underthemo_onlight@naver.com>
2026-02-13 19:02:53 +0900

    Attempted to add defaultProps, but 19 update uses 'parameter defaults'

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="민희진" />;
+  return <MyComponent />;
 }
 
 export default App;
--- study-react/src/MyComponent.js
+++ study-react/src/MyComponent.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const MyComponent = props => {
-    return <div>{props.name} 돌려줘</div>;
+const MyComponent = ({ name = "뉴진스" }) => {
+    return <div>{name} 돌려줘</div>;
 }
+
+// MyComponent.defaultProps = {
+//     name: '뉴진스'
+// }
 
 export default MyComponent;
